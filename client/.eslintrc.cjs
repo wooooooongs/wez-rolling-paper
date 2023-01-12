16b5af3654ca91ea06e5e93c645e44b87d6b69c7
wooooooongs <pires11233@gmail.com>
2023-01-13 03:10:31 +0900

    🔧 add: eslint, prettier, vite 설정파일 추가

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin: prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'warn',
+      {
+        additionalHooks: 'useRecoilCallback',
+      },
+    ],
+    indent: ['error', 2],
+    'no-unused-vars': 1,
+    'no-use-before-define': 1,
+    'no-redeclare': 1,
+    'no-console': 0,
+    'react/prop-types': 'off',
+  },
+};
