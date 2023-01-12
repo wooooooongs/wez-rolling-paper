9e3c12811bb9c4ffb4da10255c4ab96d181d8399
wooooooongs <pires11233@gmail.com>
2023-01-13 03:13:02 +0900

    🎉 add: 프로젝트 초기설정

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Router from './router';
+import { RecoilRoot } from 'recoil';
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <Router />
+    </RecoilRoot>
+  );
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import tw from 'tailwind-styled-components';
+
+const Home = () => {
+  return <div>안녕하세용</div>;
+};
+
+export default Home;
