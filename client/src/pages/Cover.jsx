a0b4aebff4a8b9c4969c14f482a952682c9a7fcb
wooooooongs <pires11233@gmail.com>
2023-02-22 18:13:29 +0900

    :lipstick: style(Cover): 비밀번호 모달창 나타날 때 배경 흐리게

--- client/src/pages/Cover.jsx
+++ client/src/pages/Cover.jsx
@@ -17,7 +17,7 @@ const Cover = () => {
       <>
         <div
           id='background'
-          className='absolute right-0 h-screen w-full bg-[#00000090]'
+          className='absolute right-0 h-screen w-full bg-[#00000070]'
           onClick={() => setOpenModal(false)}></div>
         <div className='absolute right-0 bottom-0 h-[28vh] w-full bg-white pt-[5vh]'>
           <span
@@ -65,39 +65,43 @@ const Cover = () => {
 
   return (
     <div className='relative mx-auto flex h-screen w-screen max-w-[820px] flex-col justify-between overflow-hidden'>
-      <div id='back-to-home' className='fixed'>
-        <Link to='/'>
-          <span className='inline-block h-12 w-12 '>
-            <RiHome5Line size='28' className='ml-4 mt-3' />
-          </span>
-        </Link>
-      </div>
-      <div id='happyBirthday' className='max-h-[17vh] mobileMd:max-h-[23vh]'>
-        <div className='mx-auto se:max-w-[300px] mobileSm:max-w-[400px] mobileMd:max-w-[500px]'>
-          <img src={coverHB} className='' />
+      <div className={openModal && 'blur-[2.5px]'}>
+        <div id='back-to-home' className='fixed'>
+          <Link to='/'>
+            <span className='inline-block h-12 w-12 '>
+              <RiHome5Line size='28' className='ml-4 mt-3' />
+            </span>
+          </Link>
         </div>
-      </div>
-      <div id='profileImg' className='mb-5'>
-        <div className='mx-auto se:max-w-[200px] mobileSm:max-w-[300px] mobileMd:max-w-[375px]'>
-          <img src={coding} className='aspect-square w-full rounded-full' />
+        <div id='happyBirthday' className='max-h-[17vh] mobileMd:max-h-[23vh]'>
+          <div className='mx-auto se:max-w-[300px] mobileSm:max-w-[400px] mobileMd:max-w-[500px]'>
+            <img src={coverHB} className='' />
+          </div>
         </div>
-      </div>
-      <div id='musicPlayer' className='mb-10 flex flex-col items-center gap-3'>
-        <p className='text-xl'>구혜선 - Happy Birthday To You</p>
-        <div className='flex gap-7'>
-          <span>
-            <FaPlay size='28' />
-          </span>
-          <span>
-            <FaPause size='28' />
-          </span>
-          <span>
-            <FaStop size='28' />
-          </span>
+        <div id='profileImg' className='mb-5'>
+          <div className='mx-auto se:max-w-[200px] mobileSm:max-w-[300px] mobileMd:max-w-[375px]'>
+            <img src={coding} className='aspect-square w-full rounded-full' />
+          </div>
         </div>
-      </div>
-      <div id='cake' onClick={() => setOpenModal(true)}>
-        <img src={coverExample} className='w-full' />
+        <div
+          id='musicPlayer'
+          className='mb-10 flex flex-col items-center gap-3'>
+          <p className='text-xl'>구혜선 - Happy Birthday To You</p>
+          <div className='flex gap-7'>
+            <span>
+              <FaPlay size='28' />
+            </span>
+            <span>
+              <FaPause size='28' />
+            </span>
+            <span>
+              <FaStop size='28' />
+            </span>
+          </div>
+        </div>
+        <div id='cake' onClick={() => setOpenModal(true)}>
+          <img src={coverExample} className='w-full' />
+        </div>
       </div>
       {openModal && <Modal />}
     </div>
