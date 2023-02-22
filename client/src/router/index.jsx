387119adc4a180295dde58f306eb6c5425d5e880
wooooooongs <pires11233@gmail.com>
2023-02-22 19:42:09 +0900

    :sparkles: feat(router): member-list, cover 페이지 라우터 등록

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import PaperList from '../pages/PaperList';
+import MemberList from '../pages/MemberList';
+import Cover from '../pages/Cover';
 
 const Router = createBrowserRouter([
   {
@@ -11,6 +14,14 @@ const Router = createBrowserRouter([
   {
     path: '/paper-list',
     element: <PaperList />,
+  },
+  {
+    path: '/member-list',
+    element: <MemberList />,
+  },
+  {
+    path: '/cover',
+    element: <Cover />,
   },
 ]);
 
