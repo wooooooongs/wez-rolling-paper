1ec6e7e32e131806dcf2d87842168fb2ff16fb8a
wooooooongs <pires11233@gmail.com>
2023-03-02 15:39:22 +0900

    :wrench: add(tailwind): config 파일 내 9/10 비율 및 배경화면 추가
    홈 화면 프로필 사진 비율: 9/10

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -9,6 +9,12 @@ module.exports = {
         mobileMd: { min: '769px' },
         pc: { min: '1024px' },
       },
+      aspectRatio: {
+        '9/10': '9 / 10',
+      },
+      backgroundImage: {
+        paper: "url('./src/assets/cover/background-1.png')",
+      },
     },
   },
   plugins: [
