c2c72252bb022b0368b27096152c154aca05a9c5
wooooooongs <pires11233@gmail.com>
2023-03-19 01:18:25 +0900

    :truck: rename(Background): Main -> Background

---
+++ client/src/components/Background.jsx
@@ -0,0 +1,5 @@
+import tw from 'tailwind-styled-components';
+
+const Background = tw.div`mx-auto flex h-screen w-screen max-w-[480px] flex-col overflow-hidden bg-paper bg-cover bg-no-repeat`;
+
+export { Background };
