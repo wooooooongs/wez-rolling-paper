42957a8ca601f6f2898088367b27fa5b704fc170
wooooooongs <pires11233@gmail.com>
2023-03-24 02:14:10 +0900

    ✨ feat(Cover): 선택된 멤버 데이터 PasswordModal 컴포넌트에 전달
    Issue #10

--- client/src/pages/Cover.jsx
+++ client/src/pages/Cover.jsx
@@ -3,15 +3,18 @@ import { useRecoilValue } from 'recoil';
 import { Background } from '../components/Background';
 import { Main } from '../components/cover';
 import { PasswordModal } from '../components/cover';
-import { showPasswordModalAtom } from '../recoil';
+import { currentMemberDataAtom, showPasswordModalAtom } from '../recoil';
 
 const Cover = () => {
   const showPasswordModal = useRecoilValue(showPasswordModalAtom);
+  const currentMemberData = useRecoilValue(currentMemberDataAtom);
 
   return (
     <Background>
       <Main />
-      {showPasswordModal ? <PasswordModal /> : undefined}
+      {showPasswordModal ? (
+        <PasswordModal currentMemberData={currentMemberData} />
+      ) : undefined}
     </Background>
   );
 };
