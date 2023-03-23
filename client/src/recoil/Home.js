2a2b35d42402007a9b32383ba4a21e4c1b1139c9
wooooooongs <pires11233@gmail.com>
2023-03-24 01:59:25 +0900

    ✨ feat(recoil): 선택된 멤버 데이터 저장하는 Atom 추가
    '홈 화면에서 받아온 데이터를 그대로 활용'
    Issue #10

--- client/src/recoil/Home.js
+++ client/src/recoil/Home.js
@@ -4,3 +4,8 @@ export const currentMemberNumAtom = atom({
   key: 'currentMemberNum',
   default: 0,
 });
+
+export const currentMemberDataAtom = atom({
+  key: 'currentMemberData',
+  default: {},
+});
