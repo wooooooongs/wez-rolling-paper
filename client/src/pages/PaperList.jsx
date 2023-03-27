db4e5556bfc99ae5bdddd3ce16cb292b66a0ba47
wooooooongs <pires11233@gmail.com>
2023-03-28 00:28:39 +0900

    ♻️ refactor(PaperList): 편지 모달창 Main 컴포넌트로 이동
    #789cfbb

--- client/src/pages/PaperList.jsx
+++ client/src/pages/PaperList.jsx
@@ -1,16 +1,10 @@
-import { useRecoilValue } from 'recoil';
-
 import { Background } from '../components/Background';
-import { Main, LetterModal } from '../components/PaperList/';
-import { showLetterModalAtom } from '../recoil/paper-list';
+import { Main } from '../components/PaperList/';
 
 const PaperList = () => {
-  const showLetterModal = useRecoilValue(showLetterModalAtom);
-
   return (
     <Background>
       <Main />
-      {showLetterModal && <LetterModal />}
     </Background>
   );
 };
