35a10f6da4b83d1d2948b5587b4bc2ee098bd5d5
wooooooongs <pires11233@gmail.com>
2023-03-28 17:40:06 +0900

    ♻️ refactor(Home): 멤버 데이터 전달 방식 변경, URL 파라미터 삽입
    - 기존 1: 전역 state 저장 후 가져오기
    - 변경 1: useNavigate로 state 전달
    - 기존 2: 엔터 버튼 클릭 시 URL 'cover'로 이동
    - 변경 2: '/memberId'로 이동
    Issue #12 #16

--- client/src/components/homepage/EnterButton.jsx
+++ client/src/components/homepage/EnterButton.jsx
@@ -1,23 +1,27 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { currentMemberDataAtom, currentMemberNumAtom } from '../../recoil';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { currentMemberNumAtom } from '../../recoil';
 
 const EnterButton = ({ membersData }) => {
-  const currentMemberNum = useRecoilValue(currentMemberNumAtom);
-  const setCurrentMemberData = useSetRecoilState(currentMemberDataAtom);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    setCurrentMemberData(membersData[currentMemberNum]);
-  }, []);
+  const currentMemberNum = useRecoilValue(currentMemberNumAtom);
+  const currentMemberData = membersData[currentMemberNum];
+  const currentMemberId = currentMemberData['memberId'];
+
+  const selectMember = () => {
+    navigate(`/${currentMemberId}`, {
+      state: { currentMemberData: currentMemberData },
+    });
+  };
 
   return (
     <div>
-      <Link to='cover'>
-        <button className='h-[65px] w-full rounded-xl border-b-[1px] border-[#9a9a9a] bg-white text-3xl font-semibold drop-shadow-md'>
-          ENTER
-        </button>
-      </Link>
+      <button
+        onClick={selectMember}
+        className='h-[65px] w-full rounded-xl border-b-[1px] border-[#9a9a9a] bg-white text-3xl font-semibold drop-shadow-md'>
+        ENTER
+      </button>
     </div>
   );
 };
