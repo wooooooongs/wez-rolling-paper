5534492088994cdb05a7bbc3a5923cde4ba8fc7e
wooooooongs <pires11233@gmail.com>
2023-04-01 00:08:23 +0900

    ✨ feat(recoil): 해당 멤버 편지들 및 선택된 편지 Atom 추가

--- client/src/recoil/paper-list.js
+++ client/src/recoil/paper-list.js
@@ -4,3 +4,13 @@ export const showLetterModalAtom = atom({
   key: 'showLetterModal',
   default: false,
 });
+
+export const lettersAtom = atom({
+  key: 'letters',
+  default: [],
+});
+
+export const selectedLetterAtom = atom({
+  key: 'selectedLetter',
+  default: {},
+});
