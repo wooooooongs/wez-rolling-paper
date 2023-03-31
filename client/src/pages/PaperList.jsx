053ae4b80dd7b1d91422c8d100ae1269a96d551e
wooooooongs <pires11233@gmail.com>
2023-04-01 00:21:14 +0900

    ♻️ refactor(PaperList): 전역 state 대신 전달받은 state 활용
    useLocation() 으로 state 받아온 후 Main 컴포넌트에 props로 전달
    
    Issue #12 #16

--- client/src/pages/PaperList.jsx
+++ client/src/pages/PaperList.jsx
@@ -1,13 +1,19 @@
+import { useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+
 import { Background } from '../components/Background';
 import { Main } from '../components/PaperList/';
 import { showLetterModalAtom } from '../recoil';
 import { LetterModal } from '../components/PaperList/';
 
 const PaperList = () => {
+  const location = useLocation();
+  const currentMemberData = location.state.memberData;
   const showLetterModal = useRecoilValue(showLetterModalAtom);
 
   return (
     <Background>
+      <Main memberData={currentMemberData} />
       {showLetterModal && <LetterModal />}
     </Background>
   );
