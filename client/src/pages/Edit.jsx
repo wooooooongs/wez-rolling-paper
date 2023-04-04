412f69007f908105c8bf51b9315d0fa89780dc18
wooooooongs <pires11233@gmail.com>
2023-04-04 16:11:34 +0900

    ✨ feat(Edit): 멤버 Oid Main 컴포넌트에 전달
    Issue #17

--- client/src/pages/Edit.jsx
+++ client/src/pages/Edit.jsx
@@ -1,11 +1,16 @@
+import { useLocation } from 'react-router-dom';
+
 import { Background } from '../components/Background';
 import { Header, Main } from '../components/edit';
 
 const Edit = () => {
+  const location = useLocation();
+  const currentMemberOid = location.state.memberOid;
+
   return (
     <Background>
       <Header />
-      <Main />
+      <Main memberOid={currentMemberOid} />
     </Background>
   );
 };
