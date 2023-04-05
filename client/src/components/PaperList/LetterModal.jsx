e18e3d421f1c86732f5ecf189699718c55fe3bd5
wooooooongs <pires11233@gmail.com>
2023-04-06 00:04:14 +0900

    🔧 chore(PaperList): 자잘한 수정, 설명 참고
    - 작성자명 태그 변경 p -> span
    - 작성자 비어있을 시 익명으로 표시
    - 오타 수정 selecedLetter -> selectedLetter
    - 편지 내용 줄바꿈
    - 편지 배경 색 노랑 -> 흰색에 가까운 회색

--- client/src/components/PaperList/LetterModal.jsx
+++ client/src/components/PaperList/LetterModal.jsx
@@ -11,7 +11,7 @@ import { ModalBackground } from '../Background';
 
 const LetterModal = () => {
   const setShowLetterModal = useSetRecoilState(showLetterModalAtom);
-  const selecedLetter = useRecoilValue(selectedLetterAtom);
+  const selectedLetter = useRecoilValue(selectedLetterAtom);
 
   return (
     <>
@@ -21,12 +21,12 @@ const LetterModal = () => {
           <img src={coding} className='h-full w-full rounded-lg' />
         </ProfileImg>
         <Content className=''>
-          <p className='mb-2 overflow-scroll scrollbar-hide se:max-h-[8rem] mobileSm:max-h-[17rem] mobileMd:max-h-[14.3rem]'>
-            {selecedLetter && selecedLetter['contents']}
+          <p className='mb-2 overflow-scroll whitespace-pre scrollbar-hide se:max-h-[8rem] mobileSm:max-h-[16rem] mobileMd:max-h-[14.3rem]'>
+            {selectedLetter && selectedLetter['contents']}
           </p>
-          <p className='pr-1 text-right font-semibold'>
-            {selecedLetter && selecedLetter['nickname']}
-          </p>
+          <span className='pr-1 font-semibold'>
+            {selectedLetter['nickname'] ? selectedLetter['nickname'] : '익명'}
+          </span>
         </Content>
       </Letter>
     </>
@@ -37,4 +37,4 @@ export { LetterModal };
 
 const ProfileImg = tw.div`pt-6 pb-6`;
 const Content = tw.div``;
-const Letter = tw.div`absolute top-[15vh] right-[50%] w-[21rem] translate-x-1/2 rounded-lg bg-yellow-100 px-6 shadow-2xl se:h-[30rem] mobileSm:h-[40rem] mobileMd:h-[45rem] mobileMd:w-[30rem]`;
+const Letter = tw.div`absolute top-[15vh] right-[50%] w-[21rem] translate-x-1/2 rounded-lg bg-gray-50 px-6 shadow-2xl se:h-[30rem] mobileSm:h-[40rem] mobileMd:h-[45rem] mobileMd:w-[30rem]`;
