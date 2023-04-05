adbe64b15827765e1b13fdcc0cdf9c95218ff39e
wooooooongs <pires11233@gmail.com>
2023-04-06 00:46:22 +0900

    ✨ feat(api): fetch API POST 요청 함수 추가

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -22,4 +22,28 @@ const get = async (endpoint, params = '') => {
   }
 };
 
-export { get };
+const post = async (endpoint, body) => {
+  const apiUrl = import.meta.env.VITE_SERVER_URL + endpoint;
+
+  try {
+    const res = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      const err = await res.json();
+      throw new Error(err.reason);
+    }
+
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export { get, post };
